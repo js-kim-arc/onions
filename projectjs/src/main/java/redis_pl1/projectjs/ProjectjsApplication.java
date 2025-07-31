45260440c012e92e8dee617894f7ff931526d15c
junseong kim <-67666411+js-kim-arc@users.noreply.github.com>
2025-07-31 16:01:14 +0900

    real first commit

---
+++ projectjs/src/main/java/redis_pl1/projectjs/ProjectjsApplication.java
@@ -0,0 +1,13 @@
+package redis_pl1.projectjs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectjsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectjsApplication.class, args);
+    }
+
+}
---
+++ projectjs/src/test/java/redis_pl1/projectjs/ProjectjsApplicationTests.java
@@ -0,0 +1,13 @@
+package redis_pl1.projectjs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectjsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
